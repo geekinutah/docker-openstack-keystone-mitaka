ed580d82544edeef7430fec17e756c310bc3f12a
Mike Wilson <geekinutah@gmail.com>
2018-09-11 16:49:52 -0600

    Fixed path for keystone conf

--- start_keystone.sh
+++ start_keystone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export HOST_ADDRESS=`hostname -I`
-cat /etc/keystone/keystone.conf.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /etc/keystone.keystone.conf
+cat /etc/keystone/keystone.conf.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /etc/keystone/keystone.conf
 
 cat /openrc.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /openrc
 
